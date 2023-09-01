74f99230e73ef30b27d142a8c65ef9776c744bb3
Filiph Siitam Sandström <filiph@nordcom.io>
2023-09-01 01:36:53 +0000

    API: Add `graphql` stub.

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,8 @@ import type { Logger } from 'tslog';
 import type Nordy from '@/nordy';
 import { Server } from 'http';
 import express from 'express';
+import { createHandler as graphql } from 'graphql-http/lib/use/express';
+import { schema } from '@/api/schema';
 
 export default class API {
     private readonly logger: Logger<any>;
@@ -16,10 +18,25 @@ export default class API {
         this.logger = logger;
 
         this.logger.debug(`Initializing API...`);
-        this.app = express();
+        this.app = express().use(express.urlencoded({ extended: true }));
 
         this.logger.trace(`Configuring routes...`);
-        this.app.use(express.json()).use(express.urlencoded({ extended: true }));
+        this.app
+            .use(express.json())
+            .use('/', (req, res, next) => {
+                if (req.path !== '/') return next();
+
+                return res.status(200).json({
+                    message: 'nordy'
+                });
+            })
+            .use('/heartbeat', (req, res) => res.status(200).json({ status: 'ok' }));
+
+        this.logger.trace(`Configuring graphql...`);
+        this.app.use('/graphql', graphql({ schema }));
+
+        this.logger.trace(`Configuring error routes...`);
+        this.app.use('*', (req, res) => res.status(404).json({ error: 'Not Found' }));
     }
 
     async start({ port }: { port: number }) {
